f2c7e048748051c253d2a1e4beedf0d154948001
Tyler Kuhn <contact@tyknet.xyz>
2025-01-27 02:14:15 -0500

    Added UI
    UI shows who is talking just below the chat box.

---
+++ UI/TalkingIcon.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NuclearVOIP.UI
+{
+    internal class TalkingIcon: MonoBehaviour
+    {
+        private Text? text;
+
+        private void Awake()
+        {
+            text = gameObject.AddComponent<Text>();
+            text.font = Resources.FindObjectsOfTypeAll<Font>()
+                .Where(a => a.name == "regular_cozy")
+                .First();
+
+            ContentSizeFitter fitter = gameObject.AddComponent<ContentSizeFitter>();
+        }
+
+        internal void Setup(Player player)
+        {
+            text!.text = player.GetNameOrCensored();
+        }
+    }
+}
---
+++ UI/TalkingList.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NuclearVOIP.UI
+{
+    internal class TalkingList: MonoBehaviour
+    {
+        private readonly Dictionary<Player, GameObject> entries = [];
+
+        internal void AddPlayer(Player player)
+        {
+            GameObject go = new("TalkingIcon");
+            TalkingIcon icon = go.AddComponent<TalkingIcon>();
+            icon.Setup(player);
+
+            go.transform.SetParent(gameObject.transform, false);
+
+            entries.Add(player, go);
+        }
+
+        internal void RemovePlayer(Player player)
+        {
+            if (!entries.TryGetValue(player, out GameObject go))
+                return;
+
+            Destroy(go);
+            entries.Remove(player);
+        }
+
+        private void Awake()
+        {
+            gameObject.AddComponent<VerticalLayoutGroup>();
+
+            RectTransform rtrans = gameObject.GetComponent<RectTransform>();
+            rtrans.anchorMax = new Vector2(1, 0);
+            rtrans.anchorMin = new Vector2(0, 0);
+            rtrans.pivot = new Vector2(0, 1);
+
+            CanvasGroup group = gameObject.AddComponent<CanvasGroup>();
+            group.interactable = false;
+            group.blocksRaycasts = false;
+        }
+    }
+}
